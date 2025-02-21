343911e4c08933c8b23278033d1367bf6ecc5b8b
OsatohanmwenT <osarenkhoeosato45@gmail.com>
2025-02-21 18:19:38 +0100

    modify scroll-bar

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[1fr_auto] overflow-hidden gap-3 p-4 h-screen w-full font-[family-name:var(--font-geist-sans)]">
       <div className="h-full w-full absolute z-[-1] inset-0 bg-doodle opacity-5"></div>
-      <div className="h-full z-10 w-full scroll overflow-y-scroll max-h-[720px]">
+      <div className="h-full z-10 w-full scroll overflow-y-scroll scroll max-h-[720px]">
         <div className="h-full flex flex-col max-w-4xl w-full mx-auto">
           {messages.length > 0 ? (
             <PromptList messages={messages} setMessages={setMessages} />
